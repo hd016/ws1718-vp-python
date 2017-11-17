1a07b82eb196251e294007aafa46e63ea7cbc4cf
Harun Dalici <hd016@hdm-stuttgart.de>
2017-11-17 20:43:16 +0100

    aufgabe 1-1 fertig

--- bka_parser.py
+++ bka_parser.py
@@ -12,9 +12,10 @@ import chardet      #
 import re           #
 #####################
 
-
 faelle = []
 
+global row
+row = {}
 
 def encoder():
 
@@ -28,21 +29,40 @@ def import_csv():
     with open('/Users/DHarun/PycharmProjects/Vertiefung-Programmierung/Abgabe1/tb01_FaelleGrundtabelleKreise_csv.csv', encoding='ISO-8859-1') as csvfile:
 
         next(csvfile)
-        global row
 
+        global row
         faelle = [{k: v for k, v in row.items()}
 
         for row in csv.DictReader(csvfile, delimiter=";")]
 
+        global cleaned_list
         cleaned_list = [row for row in faelle if not row["Kreisart"].isdigit()]
 
+        #for row in cleaned_list:
+         #   print(row)
 
-        for row in cleaned_list:
-            print(row)
 
     print("Die CSV wurde erfolgreich importiert.")
 
+    filter()
+
+
+def filter():
+
+    filtered_list = [row for row in cleaned_list if "LK" in row["Kreisart"] and float(row["Aufklaerungsquote"]) < 50 and not "Straftaten insgesamt" in row["Straftat"]]
+
+
+    with open('aufgabe1-1.csv', "w", newline='', encoding='ISO-8859-1') as filter_output:
+
+        fieldnames = ["Stadt-/Landkreis","Straftat","Aufklaerungsquote"]
+
+        writer = csv.DictWriter(filter_output, fieldnames=fieldnames, extrasaction='ignore', )
+        writer.writeheader()
+        for line in filtered_list:
+            writer.writerow(line)
+
+    print("Aufgabe 1-1.csv wurde exportiert.")
+
+
 #encoder() if you need encoder() then -> with open encoding=result["encoding"]
 import_csv()
-
-
