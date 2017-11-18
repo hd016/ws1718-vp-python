daa82a9069a9610c0d23d1e8718ff47e5807ce7d
Harun Dalici <hd016@hdm-stuttgart.de>
2017-11-18 19:52:55 +0100

    aufgabe 2-1 fertig

--- bka_parser.py
+++ bka_parser.py
@@ -6,16 +6,15 @@ print("")           #
 #####################
 
 print("------ Bitte Python Version 3.5.x verwenden! Ab Version 3.5.x werden geschriebene Dictionaries aus writedict als ordered dict gespeichert! ----")
+print("")
 
-#################################
-# Modul Importe                 #
-import csv                      #
-import pandas as pd             #
-import chardet                  #
-import re                       #
-from collections import defaultdict
-import functools                #
-#################################
+#####################################
+# Module #                          #
+                                    #
+import csv                          #
+import chardet                      #
+from collections import defaultdict #
+#####################################
 
 
 faelle = []
@@ -42,7 +41,7 @@ def import_csv():
         for row in csv.DictReader(csvfile, delimiter=";")]
 
         global cleaned_list
-        cleaned_list = [line for line in faelle if not line["Kreisart"].isdigit()]
+        cleaned_list = [line for line in faelle if not line["Kreisart"].isdigit() and not "Straftaten insgesamt" in line["Straftat"]]
 
 
     print("Die CSV wurde erfolgreich importiert.")
@@ -110,8 +109,143 @@ def sorted_bundesweite_berechnung():
     print("Aufgabe 1-3.csv wurde exportiert.")
 
 
+def suche_nach_daten():
+
+    while True:
+        try:
+
+            print("\nBitte geben Sie folgende Suchfelder ein:")
+            print("\n1 - Schluesse "
+                  "\n2 - Straftat"
+                  "\n3 - Gemeindeschluessel"
+                  "\n4 - Stadt-/Landkreis"
+                  "\n5 - Kreisart"
+                  "\n6 - erfasste Faelle"
+                  "\n7 - HZ nach Zensus"
+                  "\n8 - Versuche - Anzahl"
+                  "\n9 - Versuche - Anteil in %"
+                  "\n10 - mit Schusswaffe gedroht"
+                  "\n11 - mit Schusswaffe geschossen"
+                  "\n12 - aufgeklaerte Faelle"
+                  "\n13 - Aufklaerungsquote"
+                  "\n14 - Tatverdaechtige insgesamt"
+                  "\n15 - Tatverdaechtige - maennlich"
+                  "\n16 - Tatverdaechtige - weiblich"
+                  "\n17 - Nichtdeutsche Tatverdaechtige - Anzahl"
+                  "\n18 - Nichtdeutsche Tatverdaechtige - Anteil in %")
+
+
+            navigation_eingabe = int(input("\nBitte geben Sie die Menü Zahl ein:"))
+
+            if navigation_eingabe == 1:
+                searcher("Schluesse")
+
+            elif navigation_eingabe == 2:
+                searcher("Straftat")
+
+            elif navigation_eingabe == 3:
+                searcher("Gemeindeschluessel")
+
+            elif navigation_eingabe == 4:
+                searcher("Stadt-/Landkreis")
+
+            elif navigation_eingabe == 5:
+                searcher("Kreisart")
+
+            elif navigation_eingabe == 6:
+                searcher("erfasste Faelle")
+
+            elif navigation_eingabe == 7:
+                searcher("HZ nach Zensus")
+
+            elif navigation_eingabe == 8:
+                searcher("Versuche - Anzahl")
+
+            elif navigation_eingabe == 9:
+                searcher("Versuche - Anteil in %")
+
+            elif navigation_eingabe == 10:
+                searcher("mit Schusswaffe gedroht")
+
+            elif navigation_eingabe == 11:
+                searcher("mit Schusswaffe gedroht")
+
+            elif navigation_eingabe == 12:
+                searcher("aufgeklaerte Faelle")
+
+            elif navigation_eingabe == 13:
+                searcher("Aufklaerungsquote")
+
+            elif navigation_eingabe == 14:
+                searcher("Tatverdaechtige insgesamt")
+
+            elif navigation_eingabe == 15:
+                searcher("Tatverdaechtige - maennlich")
+
+            elif navigation_eingabe == 16:
+                searcher("Tatverdaechtige - weiblich")
+
+            elif navigation_eingabe == 17:
+                searcher("Nichtdeutsche Tatverdaechtige - Anzahl")
+
+            elif navigation_eingabe == 18:
+                searcher("Nichtdeutsche Tatverdaechtige - Anteil in %")
+
+            else:
+                pass
+        except ValueError:
+            print("Bitte nur Integer eingeben, die in der Menü aufgelistet sind.")
+
+
+def searcher(val):
+
+    print(val)
+    print("Bitte geben Sie ihre Suchanfrage an:")
+    search_value = input()
+
+    match = [l for l in cleaned_list if l[val] == search_value]
+
+    if any(d[val] == search_value for d in match):
+
+        print("Möchten Sie die Ergebnisse in der Command-Line anzeigen oder als CSV Speichern?")
+        csv_oder_cmd = int(input("1 - CSV , 2 - Command-Line anzeigen"))
+
+        try:
+            if csv_oder_cmd == 1:
+
+                print("Bitte geben sie den erwünschten Dateinamen an:")
+                datei_name = input()
+                with open('{}.csv'.format(datei_name), "w", newline='', encoding='ISO-8859-1') as filter_output:
+
+                    fieldnames = ["Schluesse","Straftat", "Gemeindeschluessel", "Stadt-/Landkreis", "Kreisart", "erfasste Faelle", "HZ nach Zensus", "Versuche - Anzahl",
+                                  "Versuche - Anteil in %", "mit Schusswaffe gedroht", "mit Schusswaffe geschossen", "aufgeklaerte Faelle", "Aufklaerungsquote",
+                                  "Tatverdaechtige insgesamt", "Tatverdaechtige - maennlich", "Tatverdaechtige - weiblich",
+                                  "Nichtdeutsche Tatverdaechtige - Anzahl", "Nichtdeutsche Tatverdaechtige - Anteil in %" ]
+
+                    writer = csv.DictWriter(filter_output, fieldnames=fieldnames, extrasaction='ignore', delimiter=";")
+                    writer.writeheader()
+
+                    for matches in match:
+                        writer.writerow(matches)
+                print("CSV wurde exportiert.")
+
+            elif csv_oder_cmd == 2:
+                for matches in match:
+                    print(matches)
+            else:
+                print("Bitte nur 1 oder 2 eingeben.")
+
+        except ValueError:
+            print("Bitte nur ganze Zahlen 1-2 eingeben.")
+
+
+    else:
+        print(search_value, "wurde nicht gefunden.")
+    return match
+
 
 import_csv()
-bundesweite_berechnung()
-sorted_bundesweite_berechnung()
+#bundesweite_berechnung()
+#sorted_bundesweite_berechnung()
+suche_nach_daten()
 
