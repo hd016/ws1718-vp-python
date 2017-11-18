d60863f6087a18e59b78c1ec046db5087050f5e0
Harun Dalici <hd016@hdm-stuttgart.de>
2017-11-18 13:18:04 +0100

    aufgabe 1-2.csv fertig

--- bka_parser.py
+++ bka_parser.py
@@ -7,20 +7,21 @@ print("")           #
 
 print("------ Bitte Python Version 3.5.x verwenden! Ab Version 3.5.x werden geschriebene Dictionaries aus writedict als ordered dict gespeichert! ----")
 
-#####################
-# Modul Importe     #
-import csv          #
-import pandas as pd #
-import chardet      #
-import re           #
-#####################
+#################################
+# Modul Importe                 #
+import csv                      #
+import pandas as pd             #
+import chardet                  #
+import re                       #
+from collections import defaultdict
+import functools                #
+#################################
 
 
 faelle = []
 
 global row
 row = {}
-
 
 def encoder():
 
@@ -35,16 +36,13 @@ def import_csv():
 
         next(csvfile)
 
-        global row
         faelle = [{k: v for k, v in row.items()}
+
 
         for row in csv.DictReader(csvfile, delimiter=";")]
 
         global cleaned_list
-        cleaned_list = [row for row in faelle if not row["Kreisart"].isdigit()]
-
-        #for row in cleaned_list:
-         #   print(row)
+        cleaned_list = [line for line in faelle if not line["Kreisart"].isdigit()]
 
 
     print("Die CSV wurde erfolgreich importiert.")
@@ -56,21 +54,43 @@ def filter():
 
     filtered_list = [row for row in cleaned_list if "LK" in row["Kreisart"] and float(row["Aufklaerungsquote"]) < 50 and not "Straftaten insgesamt" in row["Straftat"]]
 
-
     with open('aufgabe1-1.csv', "w", newline='', encoding='ISO-8859-1') as filter_output:
 
         fieldnames = ["Stadt-/Landkreis","Straftat","Aufklaerungsquote"]
 
         writer = csv.DictWriter(filter_output, fieldnames=fieldnames, extrasaction='ignore')
         writer.writeheader()
+
         for line in filtered_list:
             writer.writerow(line)
 
     print("Aufgabe 1-1.csv wurde exportiert.")
 
 
+def bundesweite_berechnung():
+
+    berechnen_list = [row for row in cleaned_list if row["erfasste Faelle"].isdigit() and not "Straftaten insgesamt" in row["Straftat"]]
+
+    result = defaultdict(int)
+
+    for d in berechnen_list:
+        result[d['Straftat']] += int(d['erfasste Faelle'])
+
+    bundesweite_liste = [{'Straftat': straftat, 'erfasste Faelle': value} for straftat, value in result.items()]
+
+    with open('aufgabe1-2.csv', "w", newline='', encoding='ISO-8859-1') as filter_output:
+
+        fieldnames = ["Straftat","erfasste Faelle"]
+
+        writer = csv.DictWriter(filter_output, fieldnames=fieldnames, extrasaction='ignore', delimiter=";")
+        writer.writeheader()
+
+        for line in bundesweite_liste:
+            writer.writerow(line)
+
+    print("Aufgabe 1-2.csv wurde exportiert.")
 
 
-
-#encoder() if you need encoder() then -> with open encoding=result["encoding"]
 import_csv()
+bundesweite_berechnung()
+
