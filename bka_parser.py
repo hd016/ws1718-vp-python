b5f1c8438ed73ded6809364bf434eb1c2d92f02d
Harun Dalici <hd016@hdm-stuttgart.de>
2017-11-18 23:10:18 +0100

    aufgabe 2-2 nicht stabil

--- bka_parser.py
+++ bka_parser.py
@@ -1,5 +1,5 @@
-#Aufgabe 1 einlesen
-#####################
+#Abgabe 1 Vertiefung Programmierung
+#######################
 import sys          #
 print(sys.version)  #
 print("")           #
@@ -13,7 +13,9 @@ print("")
                                     #
 import csv                          #
 import chardet                      #
-from collections import defaultdict #
+from collections import defaultdict
+import itertools
+import                      operator#
 #####################################
 
 
@@ -109,13 +111,15 @@ def sorted_bundesweite_berechnung():
     print("Aufgabe 1-3.csv wurde exportiert.")
 
 
+
 def suche_nach_daten():
 
     while True:
         try:
 
             print("\nBitte geben Sie folgende Suchfelder ein:")
-            print("\n1 - Schluesse "
+            print("\n0 - Zurück zur Hauptmenü"
+                  "\n1 - Schluesse "
                   "\n2 - Straftat"
                   "\n3 - Gemeindeschluessel"
                   "\n4 - Stadt-/Landkreis"
@@ -136,6 +140,9 @@ def suche_nach_daten():
 
 
             navigation_eingabe = int(input("\nBitte geben Sie die Menü Zahl ein:"))
+
+            if navigation_eingabe == 0:
+                hauptmenu()
 
             if navigation_eingabe == 1:
                 searcher("Schluesse")
@@ -192,7 +199,7 @@ def suche_nach_daten():
                 searcher("Nichtdeutsche Tatverdaechtige - Anteil in %")
 
             else:
-                pass
+                print("napiyon amk.")
         except ValueError:
             print("Bitte nur Integer eingeben, die in der Menü aufgelistet sind.")
 
@@ -227,6 +234,7 @@ def searcher(val):
 
                     for matches in match:
                         writer.writerow(matches)
+
                 print("CSV wurde exportiert.")
 
             elif csv_oder_cmd == 2:
@@ -238,14 +246,123 @@ def searcher(val):
         except ValueError:
             print("Bitte nur ganze Zahlen 1-2 eingeben.")
 
-
     else:
         print(search_value, "wurde nicht gefunden.")
     return match
 
 
+def filtern_von_daten():
+
+    menu_dict = {"Gemeindeschluessel":1, "erfasste Faelle":2,  "HZ nach Zensus":3, "Versuche - Anzahl":4, "Versuche - Anteil in %":5,
+                     "mit Schusswaffe gedroht":6, "mit Schusswaffe geschossen":7, "aufgeklaerte Faelle":8, "Aufklaerungsquote":9, "Tatverdaechtige insgesamt":10,
+                     "Tatverdaechtige - maennlich": 11, "Tatverdaechtige - weiblich":12,
+                     "Nichtdeutsche Tatverdaechtige - Anzahl":13, "Nichtdeutsche Tatverdaechtige - Anteil in %":14 }
+
+    menu_liste = sorted([(value,key) for (key,value) in menu_dict.items()])
+    for item in menu_liste :
+        print(item)
+
+    print("\nWelche numerische Felder möchten Sie filtern?")
+
+    print("\nGeben Sie das Feld 1 an:")
+    input_val1 = int(input())
+    val1 = menu_liste [input_val1][1]
+    print(val1)
+
+    print("\nGeben Sie das Feld 2 an:")
+    input_val2 = int(input())
+    val2 = menu_liste [input_val2][1]
+    print(val2)
+
+    print("\nSie haben folgende Felder zum Filtern ausgesucht:")
+    print(val1, "," , val2)
+
+    filterer1(val1, val2)
+
+def filterer1(val1, val2):
+
+    print("\nMit Welchen Wert möchten Sie das Erste Feld filtern?")
+    wert1 = int(input())
+
+    print("\nWelchen Operator möchten Sie verwnden?")
+    print("\n1 - <"
+          "\n2 - >"
+          "\n3 - =")
+
+    global advanced_filtered_list_val1
+    advanced_filtered_list_val1 = []
+
+    operatoren_auswahl = int(input())
+
+    if operatoren_auswahl == 1:
+        advanced_filtered_list_val1 = [row for row in cleaned_list if float(row[val1]) < wert1]
+
+    elif operatoren_auswahl == 2:
+        advanced_filtered_list_val1 = [row for row in cleaned_list if float(row[val1]) > wert1]
+
+    elif operatoren_auswahl == 3:
+        advanced_filtered_list_val1 = [row for row in cleaned_list if float(row[val1]) == wert1]
+
+    else:
+        print("Bitte nur 1-3 eingeben.")
+
+
+    print("\nMit Welchen Wert möchten Sie das Zweite Feld filtern?")
+    wert2 = int(input())
+
+    print("\nWelchen Operator möchten Sie verwnden?")
+    print("\n1 - <"
+          "\n2 - >"
+          "\n3 - =")
+
+    operatoren_auswahl = int(input())
+
+    global advanced_filtered_list_val2
+    advanced_filtered_list_val2 = []
+
+
+    if operatoren_auswahl == 1:
+        advanced_filtered_list_val2 = [row for row in cleaned_list if float(row[val2]) < wert2]
+
+    elif operatoren_auswahl == 2:
+        advanced_filtered_list_val2 = [row for row in cleaned_list if float(row[val2]) > wert2]
+
+    elif operatoren_auswahl == 3:
+        advanced_filtered_list_val2 = [row for row in cleaned_list if float(row[val2]) == wert2]
+
+    else:
+        print("Bitte nur 1-3 eingeben.")
+
+    map_filters(advanced_filtered_list_val1,advanced_filtered_list_val2)
+
+
+def map_filters(list1, list2):
+    print("\nMit Welchen Wert möchten Sie das Erste Feld filtern?")
+    print("\n1 - &"
+          "\n2 - |")
+
+    operatoren_auswahl = int(input())
+
+    if operatoren_auswahl == 1:
+        mapped_list = [row for row in list1 and list2]
+        for x in mapped_list:
+            print(x)
+    if operatoren_auswahl == 2:
+        mapped_list = [row for row in list1 or list2]
+        for x in mapped_list:
+            print(x)
+
+def hauptmenu():
+    print("ok")
+
+
+
+
+
+
 import_csv()
 #bundesweite_berechnung()
 #sorted_bundesweite_berechnung()
-suche_nach_daten()
+#suche_nach_daten()
+filtern_von_daten()
 
