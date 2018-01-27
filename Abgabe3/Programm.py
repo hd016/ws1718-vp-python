292c41b3085fdc413e993d0d7dc6bde5dd20d237
Tugay Yentur <Tuginho@users.noreply.github.com>
2018-01-27 12:08:18 +0100

    Final
    Final

--- Abgabe3/Programm.py
+++ Abgabe3/Programm.py
@@ -1,7 +1,9 @@
 from lxml import etree
 from copy import deepcopy
 import json
+import csv
 from blitzdb import Document, FileBackend
+from collections import Counter
 
 # Xml FilePath
 file_path = "dblp-2017-05-02.xml"
@@ -19,7 +21,7 @@ backend = FileBackend("./dblp-db")
 # Ende BlitzDb
 
 
-
+# Hauptmenü zur einfachen Navigation. Teilaufgaben
 def hauptmenu():
     print("\nBitte wählen Sie aus folgenden Punkten:")
 
@@ -31,7 +33,10 @@ def hauptmenu():
             "\n3 - Teilaufgabe 1.3"
             "\n4 - Teilaufgabe 2.1"
             "\n5 - Teilaufgabe 2.2"
-            "\n6 - Exit")
+            "\n6 - Teilaufgabe 3.1"
+            "\n7 - Teilaufgabe 3.2"
+            "\n8 - Teilaufgabe 3.3"
+            "\n9 - Exit")
         print("\nGeben Sie bitte ein:")
         try:
             hauptmenu_input = int(input())
@@ -50,14 +55,22 @@ def hauptmenu():
         if hauptmenu_input == 5:
             teilaufgabe2_2()
         if hauptmenu_input == 6:
+            teilaufgabe3_1()
+        if hauptmenu_input == 7:
+            teilaufgabe3_2()
+        if hauptmenu_input == 8:
+            teilaufgabe3_3()
+        if hauptmenu_input == 9:
             exit()
             break
 
-# Fertig - läuft.
+# Zähler für Inproceedings, Proceedings und Journal
 def teilaufgabe1_1():
     countInproceedings = 0
     countProceedings = 0
     countJournals = 0
+
+    print("Processing..")
 
     for event, element in etree.iterparse(file_path, load_dtd=True):
         if element.tag == 'inproceedings':
@@ -80,10 +93,12 @@ def teilaufgabe1_1():
           "journal: ", countJournals)
 
 
-# To-Do: Überprüfen ob XML valide ist und XMl nicht richtig eingerückt, warum auch immer
+# Die ersten 3 Inproceedings und Proceedings ausgeben
 def teilaufgabe1_2():
     countInproceedings = 0
     countProceedings = 0
+
+    print("Processing..")
 
     treeInproceedings = etree.Element("dblp")
     treeProceedings = etree.Element("dblp")
@@ -111,6 +126,7 @@ def teilaufgabe1_2():
         baumIn = etree.ElementTree(treeInproceedings)
         baumIn.write('sample_inproceedings.xml', pretty_print=True, xml_declaration=True, encoding='ISO-8859-1',
                      method="xml")
+        print("Sample_Inproceedings erfolgreich exportiert.")
     except (Exception):
         print("Fehler beim exportieren der XML." + Exception)
         hauptmenu()
@@ -119,6 +135,7 @@ def teilaufgabe1_2():
         baumPro = etree.ElementTree(treeProceedings)
         baumPro.write('sample_proceedings.xml', pretty_print=True, xml_declaration=True, encoding='ISO-8859-1',
                       method="xml")
+        print("Sample_Proceedings erfolgreich exportiert.")
     except (Exception):
         print("Fehler beim exportieren der XML." + Exception)
         hauptmenu()
@@ -131,6 +148,8 @@ def teilaufgabe1_3():
 
     countInproceedings = 0
     countProceedings = 0
+
+    print("Processing..")
 
     for event, element in etree.iterparse(file_path, load_dtd=True):
         if element.tag == 'inproceedings':
@@ -246,6 +265,7 @@ def teilaufgabe1_3():
     try:
         with open('sample_inproceedings.json', 'w', encoding='ISO-8859-1') as outfile:
             json.dump(inproceedingsList, outfile, indent=4)
+            print("Sample_Inproceedings erfolgreich exportiert.")
     except (Exception):
         print("Fehler beim speichern als JSON." + Exception)
         hauptmenu()
@@ -253,15 +273,16 @@ def teilaufgabe1_3():
     try:
         with open('sample_proceedings.json', 'w', encoding='ISO-8859-1') as outfile:
             json.dump(proceedingsList, outfile, indent=4)
+            print("Sample_Proceedings erfolgreich exportiert.")
     except (Exception):
         print("Fehler beim speichern als JSON." + Exception)
         hauptmenu()
 
-# Evtl. nicht richtig für BlitzDB -> Dokumente verschieden benennen?
+# Import in BlitzDb
 def teilaufgabe2_1():
 
     try:
-        print("Parsing ...")
+        print("Parsing a large file, time for coffee? :-) ...")
         parser = etree.XMLParser(dtd_validation=True)
         tree = etree.parse(file_path, parser)
         root = tree.getroot()
@@ -368,9 +389,12 @@ def teilaufgabe2_1():
         e.clear()
 
     # Commit, damit die Dokumente auf die Disc gespeichert werden
+    print("Erfolgreich alle Daten in die Datenbank geschrieben.")
     backend.commit()
 
+# Vergleich von Inproceedings mit Proceedings und aktualisiere die Inproceedings in der Datenbank
 def teilaufgabe2_2():
+    print("Processing..")
     inproceedings = backend.filter(Inproceedings, {})
 
     for inproceeding in inproceedings:
@@ -395,9 +419,116 @@ def teilaufgabe2_2():
 
     backend.commit()
 
+# Suche nach einem bestimmten Author
 def teilaufgabe3_1():
-    print()
+    editors = backend.filter(Inproceedings, {'proc:editor' : 'Michael L. Brodie'})
+    editorList = []
 
+    for editor in editors:
+        try:
+            e = [editor.author, editor.title, editor.pages, editor['proc:editor'], editor['proc:title']]
+            editorList.append(e)
+        except editor.DoesNotExist:
+            pass
+        except editor.MultipleDocumentsReturned:
+            pass
+
+    # Export als CSV
+    try:
+        with open('editor_MichaelLBrodie.csv', 'w') as csvfile:
+            csv_out = csv.writer(csvfile)
+            csv_out.writerow(['author', 'title', 'pages', 'proc:editor', 'proc:title'])
+            for row in editorList:
+                csv_out.writerow(row)
+
+            print("Die CSV wurde erfolgreich exportiert.")
+    except (Exception):
+        print(Exception)
+
+# Suche nach Konferenzbändern die länger als 10 Seiten sind
+def teilaufgabe3_2():
+    pages = backend.filter(Inproceedings, {})
+    inproceedings = []
+
+    for page in pages:
+        try:
+            # Seitenanzahl abrufen und als Integer umwandeln
+            laenge = len(page.pages)
+            x = page.pages.find("-")
+            zahl1 = []
+            zahl2 = []
+
+            if(x != 0 and laenge != 0):
+                for y in range(laenge):
+                    if(y != x):
+                        zahl1.append(page.pages[y])
+                    if(y == x):
+                        break
+                for y in range(laenge):
+                    if(y > x):
+                        zahl2.append(page.pages[y])
+
+                num = map(str, zahl1)
+                num = ''.join(num)
+                if (num.isdigit()):
+                    seitenZahl1 = int(num)
+
+                num2 = map(str, zahl2)
+                num2 = ''.join(num2)
+                if(num2.isdigit()):
+                    seitenZahl2 = int(num2)
+
+            seitenAnzahl = seitenZahl2 - seitenZahl1
+            # Ende
+
+            # Ausgabe der Konferenzbänder die länger als 10 Seiten sind
+            if(seitenAnzahl > 10):
+                if('proc:editor' in page): # Manche Inproceedings erhalten kein proc, hierauf wird geprüft ob dieser Key im Inproceedings Dokument vorhanden ist
+                    p = [page.author, page.title, page.pages, page['proc:editor'], page['proc:title']]
+                    inproceedings.append(p)
+
+        except page.DoesNotExist:
+            pass
+        except page.MultipleDocumentsReturned:
+            pass
+
+    # Export als CSV
+    try:
+        with open('konferenzbaenderMin10Seiten.csv', 'w') as csvfile:
+            csv_out = csv.writer(csvfile)
+            csv_out.writerow(['author', 'title', 'pages', 'proc:editor', 'proc:title'])
+            for row in inproceedings:
+                csv_out.writerow(row)
+
+            print("Die CSV wurde erfolgreich exportiert.")
+    except (Exception):
+        print(Exception)
+
+# Anzahl der Authoren die in den Aufsätzen vorkommen, absteigend sortiert.
+def teilaufgabe3_3():
+    inproceedings = backend.filter(Inproceedings, {})
+    listeAuthoren = []
+
+
+    for inproceeding in inproceedings:
+        for author in inproceeding.author:
+            listeAuthoren.append(author)
+
+    # Zuerst die Elemente die mehrfach vorkommen zuammen Zählen, anschließend die Liste absteigend nach Anzahl sortieren
+    countList = Counter(listeAuthoren).items()
+    sortedList = sorted(countList, key=lambda author: author[1], reverse=True)
+
+    # Export als CSV
+    try:
+        with open('authoren.csv', 'w') as csvfile:
+            csv_out = csv.writer(csvfile)
+            csv_out.writerow(['author', 'count'])
+            for row in sortedList:
+                csv_out.writerow(row)
+
+            print("Die CSV wurde erfolgreich exportiert.")
+    except (Exception):
+        print(Exception)
 
 
 hauptmenu()
